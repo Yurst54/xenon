9618d0d8ae8b7cb7caa6d776164bf8bff66ccb0f
Yurst54 <60783921+Yurst54@users.noreply.github.com>
2020-02-14 15:35:27 +0400

    Create invite.js

---
+++ Commands/invite.js
@@ -0,0 +1,26 @@
+const { RichEmbed } = require("discord.js");
+module.exports = class invite {
+    constructor() {
+        this.name = "invite",
+        this.alias = [],
+        this.usage = "!invite"
+    }
+
+    async run(client, message, args) {
+        try{
+            let info = client.emojis.get("655091815401127966") || "ℹ️"
+            client.generateInvite(['ADMINISTRATOR']).then(link => {
+            let inviteEmbed = new RichEmbed()
+            .setTitle(`${info} Info`)
+            .setDescription(`**İnvite Xenon**
+[Xenon ](${link})
+[Xenon](https://discordapp.com/oauth2/authorize?client_id=666655096041373737&scope=bot&permissions=8) 
+`)
+            .setColor("#5DBCD2")
+            message.channel.send(inviteEmbed);
+            })
+        }catch(e) {
+            throw e;
+        }
+    }
+}
